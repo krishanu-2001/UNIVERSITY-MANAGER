885dcbd830818806c3dbbeeeb49a7d53941a6126
KRISHANU SAINI <56930593+krishanu-2001@users.noreply.github.com>
2020-09-23 23:08:19 +0530

    student module done

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import *
 from flask_mysqldb import MySQL
 import yaml
+import hashlib
 
 app = Flask(__name__)
+app.secret_key = "abc"
 
 # Configure db
 db = yaml.load(open('db.yaml'))
@@ -26,19 +28,23 @@ def login():
     if request.method == 'POST':
         # Fetch form data
         userDetails = request.form
-        name = userDetails['name']
-        email = userDetails['email']
+        rollno = userDetails['rollno']
+        password = userDetails['password']
+        hashedpassword = hashlib.md5(password.encode()).hexdigest()
         cur = mysql.connection.cursor()
-        cur.execute("SELECT COUNT(*) FROM users WHERE name = '%s' "% (name))
-        rv = cur.fetchall();
-        flag = (rv[0][0]);
+        cur.execute("SELECT COUNT(*), password, sname FROM student WHERE rollno = '%s' "% (rollno))
+        rv = cur.fetchall()
+        flag = (rv[0][0])
+        curpassword = (rv[0][1])
+        name = (rv[0][2])
         mysql.connection.commit()
         cur.close()
         #give access here!!
-        if(flag > 1):
+        if(flag >= 1 and hashedpassword == curpassword):
+            session['rollno'] = rollno
             return redirect(url_for('users', name = name))
         else:
-            flash = "wrong id!"
+            flash = "wrong id or password!"
             flag = 0
     if flag != 0:
         flash = ""
@@ -46,26 +52,97 @@ def login():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    flash = ""
     if request.method == 'POST':
         # Fetch form data
         userDetails = request.form
+        rollno = userDetails['rollno']
         name = userDetails['name']
-        email = userDetails['email']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users(name, email) VALUES(%s, %s)",(name, email))
-        mysql.connection.commit()
-        cur.close()
-        return redirect('/users')
-    return render_template('login.html')
+        password = userDetails['password']
+        hashedpassword = hashlib.md5(password.encode()).hexdigest()
+        confpassword = userDetails['confpassword']
+        if (len(rollno) == 0 or len(password) == 0 or len(name) == 0):
+            flash = "null values encountered"
+
+        elif (confpassword != password):
+            #pass
+            flash = "check password"
+        else:
+            cur = mysql.connection.cursor()
+            cur.execute("SELECT COUNT(*) FROM student WHERE rollno = '%s' "% (rollno))
+            rv = cur.fetchall();
+            flag = (rv[0][0]);
+            if(flag >= 1):
+                flash = "already enrolled!"
+                mysql.connection.commit()
+                cur.close()
+            else:
+                cur.execute("INSERT INTO student(rollno, sname, password) VALUES(%s, %s, %s)",(rollno, name, hashedpassword))
+                mysql.connection.commit()
+                cur.close()
+                session['rollno'] = rollno
+                return redirect(url_for('users', name = name))
+    return render_template('signup.html', flash = flash)
+
+
+@app.route('/logout')
+def logout():
+    flash = "logout successfully!"
+    if(session.get('rollno')):
+        session.pop('rollno')
+        print(session['rollno'])
+    return render_template('index.html', flash = flash)
 
 
 @app.route('/users')
 def users():
     cur = mysql.connection.cursor()
-    resultValue = cur.execute("SELECT * FROM users")
-    if resultValue > 0:
-        userDetails = cur.fetchall()
-        return render_template('users.html',userDetails=userDetails)
+    if(session.get('rollno')):
+        userDetails = session['rollno']
+        rollno = session['rollno']
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM student WHERE rollno = '%s' "% (rollno))
+        rv = cur.fetchall();
+        cur.close()
+    else:
+        userDetails = "Not Authorized to access"
+
+    return render_template('users.html',userDetails=userDetails)
+
+
+@app.route('/studentprofile', methods=['GET','POST'])
+def studentprofile():
+    rollno = session['rollno']
+    flash = ""
+    flag = 1
+    if request.method == 'POST':
+        # Fetch form data
+        userDetails = request.form
+        rollno = userDetails['rollno']
+        phone = userDetails['phone']
+        address = userDetails['address']
+        cpi = userDetails['cpi']
+        _class = userDetails['class']
+        program = userDetails['program']
+        email = userDetails['email']
+        dob_dd = userDetails['dob_dd']
+        dob_mm = userDetails['dob_mm']
+        dob_yy = userDetails['dob_yy']
+        if(len(phone) > 0 and len(address) > 0 and len(_class) > 0 and len(program) > 0 and len(email) > 0):
+            #pass
+            cur = mysql.connection.cursor()
+            cur.execute("UPDATE student SET phone = %s, address = %s, cpi = %s, class = %s, program = %s, email = %s, dob_dd = %s, dob_mm = %s, dob_yy = %s WHERE rollno = %s",(phone, address, cpi, _class, program, email, dob_dd, dob_mm, dob_yy, rollno)  )
+            mysql.connection.commit()
+            cur.close()
+        else:
+            flash = "wrong id or password!"
+            flag = 0
+        #give access here!!
+
+    if flag != 0:
+        flash = ""
+    return render_template('studentprofile.html', rollno = rollno)
+
 
 if __name__ == '__main__':
     flag = 0
